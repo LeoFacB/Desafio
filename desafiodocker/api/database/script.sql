038945dcffb15b88d9a5b169d06a3d6f2e510bdc
Leonardo Bonetti <leonardofbonetti@gmail.com>
2024-02-06 16:37:07 -0300

    Comentando o codigo

--- desafiodocker/api/database/script.sql
+++ desafiodocker/api/database/script.sql
@@ -1,6 +1,8 @@
+/*Cria um banco de dados chamado funcionarios e o seleciona*/
 CREATE DATABASE IF NOT EXISTS funcionarios;
 USE funcionarios;
 
+/*Dentro do banco cria uma tabela de funcionarios com as informacoes necessarias*/
 CREATE TABLE IF NOT EXISTS info(
     Matricula INT(6) AUTO_INCREMENT NOT NULL,
     Nome CHAR(20) NOT NULL,
@@ -11,7 +13,7 @@ CREATE TABLE IF NOT EXISTS info(
 
 );
 
-
+/*Insere na tabela informacoes para serem usadas como exemplo*/
 INSERT INTO info (Nome, Sobrenome, Cargo, Salario) VALUES ('Alberto', 'Costa', 'Analista de Sistemas', 13456);
 INSERT INTO info (Nome, Sobrenome, Cargo, Salario) VALUES ('Ana', 'Mendes', 'Desenvolvedora Web', 12000);
 INSERT INTO info (Nome, Sobrenome, Cargo, Salario) VALUES ('Julia', 'Pereira', 'Analista de Sistemas', 13456);
--- desafiodocker/api/src/index.js
+++ desafiodocker/api/src/index.js
@@ -2,11 +2,12 @@ var express = require ('express');
 var mysql2 = require('mysql2/promise');
 require('dotenv').config();
 
-
+/*Inicializa o express e indica qual porta a aplicacao esta ouvindo*/
 const app = express();
 const server = app.listen(3000, '0.0.0.0');
 
-//Funcao para se conectar-se ao banco de dados
+/*Funcao para se conectar-se ao banco de dados.
+process.env.<variavel> indica a variavel localizado no arquivo .env*/
 const connection = () => mysql2.createConnection({
     host: 'db_docker',
     port: '3306',
@@ -15,10 +16,12 @@ const connection = () => mysql2.createConnection({
     database: 'funcionarios'
 })
 
+/*Envia uma mensagem para indicar o funcionamento*/
 app.get('/', function (req, res) {
   res.send('Pagina pronta')
 })
 
+/*Puxa do banco de dados as infos da tabela e disponibiliza-as*/
 app.get('/funcionarios', async function(req, res) {
     
   let con = await connection()
